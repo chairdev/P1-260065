ef0df3851523d98f22f454ca30ce2edf4ed872f7
Tirell <38292283+chairdev@users.noreply.github.com>
2026-09-10 11:48:15 +0200

    Clean up code so it's actually readable

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -26,30 +26,44 @@ function draw()
   background(220);
   textSize(15);
 
-  //#region Name
-  fill("black");
-  text("1", xAnchor, calculateYAnchor(1), 70, 80);
-  //"btch pls... you know who tf i am" - a very wise woman (natalie nunn)
-  text("Tirell Benard", calculateXAnchor(1), calculateYAnchor(1), 100, 80);
-  //#endregion
+  DrawName();
+  DrawFlag();
+  DrawCheckerboard();
+  DrawTransparentHouse();
+  DrawTrafficLight();
+  DrawDice();
+}
 
-  //#region Flag
+function DrawName()
+{
   fill("black");
-  text("2", xAnchor, calculateYAnchor(2), 70, 80);
+  text("1", xAnchor, CalcYAnchor(1), 70, 80);
+  //"btch pls... you know who tf i am" - a very wise woman (natalie nunn)
+  text("Tirell Benard", CalcXAnchor(1), CalcYAnchor(1), 100, 80);
+}
+
+function DrawFlag()
+{
+  fill("black");
+  text("2", xAnchor, CalcYAnchor(2), 70, 80);
 
   //green white green
   noStroke();
   fill(roffaGreen);
-  rect(calculateXAnchor(2), calculateYAnchor(2) + stripeHeight, 100, stripeHeight)
+  rect(CalcXAnchor(2), CalcYAnchor(2) + stripeHeight, 100, stripeHeight)
   fill("white");
-  rect(calculateXAnchor(2), calculateYAnchor(2) + stripeHeight * 2, 100, stripeHeight)
+  rect(CalcXAnchor(2), CalcYAnchor(2) + stripeHeight * 2, 100, stripeHeight)
   fill(roffaGreen);
-  rect(calculateXAnchor(2), calculateYAnchor(2) + stripeHeight * 3, 100, stripeHeight)
-  //#endregion
+  rect(CalcXAnchor(2), CalcYAnchor(2) + stripeHeight * 3, 100, stripeHeight)
+}
 
-  //#region Checkerboard
+function DrawCheckerboard()
+{
+  // this is insufferable and i dont feel like doing it rn so im writing comments
+  // i forgot how nice it is to pass time this way
+
   fill("black");
-  text("3", xAnchor, calculateYAnchor(5), 70, 80);
+  text("3", xAnchor, CalcYAnchor(5), 70, 80);
   
   // Black squares
   stroke(1);
@@ -57,86 +71,85 @@ function draw()
 
   //Drawing them using offsets (math blegh) because its ez
   fill("black");
-  square(calculateXAnchor(3), calculateYAnchor(5), checkerboardSize);
-  square(calculateXAnchor(3) + checkerboardSize, calculateYAnchor(5) + checkerboardSize, checkerboardSize);
-  square(calculateXAnchor(3) + checkerboardSize*2, calculateYAnchor(5) + checkerboardSize * 2, checkerboardSize);
-  square(calculateXAnchor(3)+checkerboardSize*2, calculateYAnchor(5), checkerboardSize);
-  square(calculateXAnchor(3), calculateYAnchor(5)+checkerboardSize*2, checkerboardSize);
+  square(CalcXAnchor(3), CalcYAnchor(5), checkerboardSize);
+  square(CalcXAnchor(3) + checkerboardSize, CalcYAnchor(5) + checkerboardSize, checkerboardSize);
+  square(CalcXAnchor(3) + checkerboardSize*2, CalcYAnchor(5) + checkerboardSize * 2, checkerboardSize);
+  square(CalcXAnchor(3)+checkerboardSize*2, CalcYAnchor(5), checkerboardSize);
+  square(CalcXAnchor(3), CalcYAnchor(5)+checkerboardSize*2, checkerboardSize);
 
   // White squares
   fill("white");
-  square(calculateXAnchor(3), calculateYAnchor(5)+checkerboardSize, checkerboardSize);
-  square(calculateXAnchor(3) + checkerboardSize*2, calculateYAnchor(5) + checkerboardSize, checkerboardSize);
-  square(calculateXAnchor(3) + checkerboardSize, calculateYAnchor(5), checkerboardSize);
-  square(calculateXAnchor(3) + checkerboardSize, calculateYAnchor(5) + checkerboardSize*2, checkerboardSize);
-  //#endregion
+  square(CalcXAnchor(3), CalcYAnchor(5)+checkerboardSize, checkerboardSize);
+  square(CalcXAnchor(3) + checkerboardSize*2, CalcYAnchor(5) + checkerboardSize, checkerboardSize);
+  square(CalcXAnchor(3) + checkerboardSize, CalcYAnchor(5), checkerboardSize);
+  square(CalcXAnchor(3) + checkerboardSize, CalcYAnchor(5) + checkerboardSize*2, checkerboardSize);
+}
 
-  // this is insufferable and i dont feel like doing it rn so im writing comments
-  // i forgot how nice it is to pass time this way
-
-  //#region Transparent house
+function DrawTransparentHouse()
+{
   strokeWeight(0);
   fill("black");
-  text("4", xAnchor, calculateYAnchor(7), 70, 80);
+  text("4", xAnchor, CalcYAnchor(7), 70, 80);
 
   strokeWeight(5)
   fill(0, 0, 0, 0);
-  triangle(calculateXAnchor(4), calculateYAnchor(8), calculateXAnchor(7) + houseWidth, calculateYAnchor(8), calculateXAnchor(5) + houseWidth / 2, calculateYAnchor(8) - houseHeight/2);
-  rect(calculateXAnchor(5), calculateYAnchor(8), houseWidth, houseHeight);
-  //#endregion
+  triangle(CalcXAnchor(4), CalcYAnchor(8), CalcXAnchor(7) + houseWidth, CalcYAnchor(8), CalcXAnchor(5) + houseWidth / 2, CalcYAnchor(8) - houseHeight/2);
+  rect(CalcXAnchor(5), CalcYAnchor(8), houseWidth, houseHeight);
+}
 
-  //#region Traffic Light
+function DrawTrafficLight()
+{
   strokeWeight(0);
   fill("black");
-  text("5", calculateXAnchor(200), calculateYAnchor(1), 70, 80);
+  text("5", CalcXAnchor(200), CalcYAnchor(1), 70, 80);
 
   noStroke()
   fill("gray");
-  rect(calculateXAnchor(210), calculateYAnchor(2), 50, 140);
+  rect(CalcXAnchor(210), CalcYAnchor(2), 50, 140);
 
   fill("red")
-  circle(calculateXAnchor(200)+signalSize, calculateYAnchor(2)+signalSize/1.5, signalSize);
+  circle(CalcXAnchor(200)+signalSize, CalcYAnchor(2)+signalSize/1.5, signalSize);
 
   fill("orange")
-  circle(calculateXAnchor(200)+signalSize, calculateYAnchor(3.5)+signalSize/1.5, signalSize);
+  circle(CalcXAnchor(200)+signalSize, CalcYAnchor(3.5)+signalSize/1.5, signalSize);
 
   fill("green")
-  circle(calculateXAnchor(200)+signalSize, calculateYAnchor(5)+signalSize/1.5, signalSize);
+  circle(CalcXAnchor(200)+signalSize, CalcYAnchor(5)+signalSize/1.5, signalSize);
 
   fill("gray");
-  rect(calculateXAnchor(200)+signalSize/1.5, calculateYAnchor(6.5), 25, 40); 
-  //#endregion
+  rect(CalcXAnchor(200)+signalSize/1.5, CalcYAnchor(6.5), 25, 40); 
+}
 
-  //#region Dice
+function DrawDice()
+{
   fill("black");
-  text("6", calculateXAnchor(200), calculateYAnchor(8), 70, 80);
+  text("6", CalcXAnchor(200), CalcYAnchor(8), 70, 80);
 
   stroke(1);
   strokeWeight(3);
   fill("white");
-  square(calculateXAnchor(210), calculateYAnchor(9), 100, 10);
+  square(CalcXAnchor(210), CalcYAnchor(9), 100, 10);
 
   stroke(1)
   strokeWeight(3)
   fill("black")
 
   //This'll draw a 6. Yes, I'm lazy. How'd you know?
-  circle(calculateXAnchor(210)+diceCircleSize, calculateYAnchor(9)+diceCircleSize, diceCircleSize);
-  circle(calculateXAnchor(210)+diceCircleSize, calculateYAnchor(10)+diceCircleSize, diceCircleSize);
-  circle(calculateXAnchor(210)+diceCircleSize, calculateYAnchor(11)+diceCircleSize, diceCircleSize);
+  circle(CalcXAnchor(210)+diceCircleSize, CalcYAnchor(9)+diceCircleSize, diceCircleSize);
+  circle(CalcXAnchor(210)+diceCircleSize, CalcYAnchor(10)+diceCircleSize, diceCircleSize);
+  circle(CalcXAnchor(210)+diceCircleSize, CalcYAnchor(11)+diceCircleSize, diceCircleSize);
 
-  circle(calculateXAnchor(270)+diceCircleSize, calculateYAnchor(9)+diceCircleSize, diceCircleSize);
-  circle(calculateXAnchor(270)+diceCircleSize, calculateYAnchor(10)+diceCircleSize, diceCircleSize);
-  circle(calculateXAnchor(270)+diceCircleSize, calculateYAnchor(11)+diceCircleSize, diceCircleSize);
-  //#endregion
+  circle(CalcXAnchor(270)+diceCircleSize, CalcYAnchor(9)+diceCircleSize, diceCircleSize);
+  circle(CalcXAnchor(270)+diceCircleSize, CalcYAnchor(10)+diceCircleSize, diceCircleSize);
+  circle(CalcXAnchor(270)+diceCircleSize, CalcYAnchor(11)+diceCircleSize, diceCircleSize);
 }
 
-function calculateXAnchor(num)
+function CalcXAnchor(num)
 {
   return xAnchor + (num + 15);
 }
 
-function calculateYAnchor(num)
+function CalcYAnchor(num)
 {
   return yAnchor + (num * 30);
 }
