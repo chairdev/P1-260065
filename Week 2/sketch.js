a01bd3ec969f56cc0e7b84351ccf8f9601a662d6
Tirell <38292283+chairdev@users.noreply.github.com>
2026-09-16 10:58:06 +0200

    Start on week 2. Finish road rendering code

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,34 @@
+const canvasWidth = 800;
+const canvasHeight = 600;
+//wow week 2!! aren't we all so excited to work with p5.js more
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
 }
 
 function draw() {
   background(220);
+  //DrawBackground();
+  DrawRoad()
+}
+
+function DrawRoad()
+{
+  const roadHeight = 200;
+  const numStripes = 10;
+  const stripeWidth = 50;
+  const stripeHeight = 12;
+  const stripeY = roadHeight + 290;
+
+  fill("gray");
+  rect(0, roadHeight*2, canvasWidth, roadHeight)
+
+  //use a for loop to draw the stripes with an offset
+  fill("white");
+
+  for (let i = 0; i < numStripes; i++) 
+  {
+    let stripeX = stripeWidth * 2 * i;
+    rect(stripeX, stripeY, stripeWidth, stripeHeight, 20)
+    
+  }
 }
